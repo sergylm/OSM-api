bba234cb7b52a930960d1b91621ee9b62b9646b7
sergylopez21 <sergy_lopez_21@hotmail.com>
2021-07-12 14:46:41 +0200

    Download button

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask, request, flash, redirect, url_for
+from flask import Flask, request, flash, redirect, url_for, Response
+from flask.helpers import send_file
 from flask.templating import render_template
 from werkzeug.utils import secure_filename
+import zipfile
+import time
 
 UPLOAD_FOLDER = '/uploads'
 
@@ -17,7 +20,7 @@ def prueba():
     return render_template('base.html')
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
+def upload():
     if 'file' not in request.files:
         print('No file part')
         return redirect(request.url)
@@ -27,10 +30,26 @@ def upload_file():
     if file and allowed_file(file.filename):
         secfilename = secure_filename(file.filename)
         filename = secfilename.rsplit('.')[0]
-        file.save(secfilename)
-        os.system('java -Xmx512m -jar OSM2World/OSM2World.jar -i {} -o {}'.format(secfilename, filename + '.obj'))
+        file.save('uploads/' + secfilename)
         return 'OK', 200
     return 'Format not valid', 400
+
+@app.route('/download', methods=['GET'])
+def download():
+    return send_file('objects.zip', mimetype='zip', attachment_filename='objects.zip', as_attachment=True)
+
+@app.route('/convert', methods=['POST'])
+def osm2obj():
+    files = os.listdir('uploads')
+    for file in files:
+        os.system('java -Xmx512m -jar OSM2World/OSM2World.jar -i {} -o {}'.format('uploads/' + file, 'downloads/' + file.rsplit('.')[0] + '.obj'))
+    zip(os.listdir('downloads'))
+    return 'Done'
+
+def zip(path):
+    with zipfile.ZipFile('objects.zip', 'w' , zipfile.ZIP_DEFLATED) as zf:
+        for file in path:
+           zf.write('downloads/' +  file, file) 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() == 'osm'
--- static/scripts.js
+++ static/scripts.js
@@ -1,16 +1,12 @@
-var myDropzone = new window.Dropzone('div#update', {url: "/"});
-
-// dropCOntainer.ondraggover = dropCOntainer.ondragenter = function(e){
-//     e.preventDefault();
-// };
-
-// dropCOntainer.ondrop = function(e){
-//     fileInput.files = e.dataTransfer.files;
-//     const dt = new DataTransfer();
-
-//     dt.items.add(e.e.dataTransfer.files[0]);
-//     dt.items.add(e.e.dataTransfer.files[3]);
-//     fileInput.files = dt.files;
-
-//     e.preventDefault();
-// }
+$('#btn').click(function(){
+    var btn = $(this);
+    if(btn.hasClass('convert')){
+        btn.text('Converting').prop('disabled', true)
+        $.post('/convert').then(function(){
+            btn.prop('disabled', false).text('Download files').removeClass('convert').addClass('download')
+        })
+    } else{
+        btn.text('Downloaded')
+        window.location = '/download'
+    }
+})
